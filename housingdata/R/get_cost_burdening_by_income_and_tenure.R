cab40d4b3c588ef890b56b973eae02b4c2feb9d7
buchmayne <michaelmbuchman@gmail.com>
2021-02-08 18:17:17 -0800

    created first function to calculate cost burdening by tenure and household income

--- housingdata/R/get_cost_burdening_by_income_and_tenure.R
+++ housingdata/R/get_cost_burdening_by_income_and_tenure.R
@@ -1,16 +1,39 @@
+#' Get Cost Burdening by Income and Tenure
+#'
+#' This function uses the census API to pull down cost burdening data from table B25106 of the ACS. It then tidies the returned data.
+#' @param year Defaults to 2019, value passed to the census API call
+#' @param geography The unit of geography to return data for, options are "state", "county", "place" and "tract". This value is passed to the census API call
+#' @param state U.S. state to return data for, value passed to the census API call. Defaults to NULL
+#' @param county Defaults to NULL, option to return tract or county data from a specific county. Can't return place data from a specific county. This value is passed to census API call
+#' @param survey Defaults to "acs5", the version of the ACS to return. Options are "acs5" and "acs1" for either the five-year or one-year ACS survey. Value passed to census API call
+#' @export
+#' @examples
+#' get_cost_burdening_by_income_and_tenure(year = 2019, geography = "county", state = "ID", survey = "acs5")
+
+
 library(dplyr)
 library(stringr)
 library(tidycensus)
 
 
-get_cost_burdening_by_income_and_tenure <- function(year = 2019, geography, state, county = NULL, survey = "acs5") {
+get_cost_burdening_by_income_and_tenure <- function(year = 2019, geography, state = NULL, county = NULL, survey = "acs5") {
+
+  # restrict queries to state, county, place, and tract
+  if (!geography %in% c("state", "county", "place", "tract")) {
+    stop("state, county, place, and tract are the only valid inputs to geography")
+  }
+
+  # error handling for survey parameter
+  if (!survey %in% c("acs5", "acs1")) {
+    stop("acs5 and acs1 are the only valid inputs to survey")
+  }
 
   # get census variable names
-  acs_vars <- readRDS("data/acs_variables.rds")
+  acs_vars <- tidycensus::load_variables(year = year, dataset = survey, cache = TRUE)
   cost_burdening_tbl <- "B25106"
 
   # pull down data via census api
-  cb <- tidycensus::get_acs(
+  raw_cb <- tidycensus::get_acs(
     geography = geography,
     table = cost_burdening_tbl,
     survey = survey,
@@ -20,14 +43,16 @@ get_cost_burdening_by_income_and_tenure <- function(year = 2019, geography, stat
     cache_table = TRUE
   )
 
-  # join variable names and relabel
-  cb <- dplyr::inner_join(cb, acs_vars, by = c("variable" = "name"))
 
-  cb <- cb[c("GEOID", "NAME", "estimate", "moe", "label")]
-  colnames(cb) <- c("GEOID", "NAME", "housing_units", "moe", "label")
+  # join variable names and relabel
+  raw_cb <- dplyr::inner_join(raw_cb, acs_vars, by = c("variable" = "name"))
+
+  raw_cb <- raw_cb[c("GEOID", "NAME", "estimate", "moe", "label")]
+  colnames(raw_cb) <- c("GEOID", "NAME", "housing_units", "moe", "label")
+
 
   # drop row containing totals and subtotals, create tenure column
-  cb <- cb %>%
+  cb <- raw_cb %>%
     dplyr::filter(!label %in% c("Estimate!!Total:", "Estimate!!Total:!!Owner-occupied housing units:", "Estimate!!Total:!!Renter-occupied housing units:")) %>%
     dplyr::mutate(label = gsub("Estimate!!Total:!!", "", label)) %>%
     dplyr::mutate(tenure = dplyr::if_else(
@@ -42,6 +67,7 @@ get_cost_burdening_by_income_and_tenure <- function(year = 2019, geography, stat
     ) %>%
     dplyr::mutate(label = gsub("Owner-occupied housing units:!!", "", gsub("Renter-occupied housing units:!!", "", label))) %>%
     dplyr::filter(!stringr::str_detect(label, ":$"))
+
 
   # extract household income and housing costs from label and create new columns
   cb <- cb %>%
@@ -68,10 +94,22 @@ get_cost_burdening_by_income_and_tenure <- function(year = 2019, geography, stat
     dplyr::select(-label)
 
 
+  # check that totals match before and after modification
+  raw_total_units <- raw_cb %>%
+    dplyr::filter(label == "Estimate!!Total:") %>%
+    dplyr::select(GEOID, housing_units)
+
+  check_totals_match <- cb %>%
+    dplyr::group_by(GEOID) %>%
+    dplyr::summarise(transformed_total = sum(housing_units)) %>%
+    dplyr::ungroup() %>%
+    dplyr::inner_join(raw_total_units) %>%
+    dplyr::filter(housing_units != transformed_total)
+
+  stopifnot(nrow(check_totals_match) == 0)
+
+
   return(cb)
 
 
 }
-
-
-
